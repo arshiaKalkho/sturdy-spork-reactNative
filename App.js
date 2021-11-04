1e6a2db6d25da6baa7d6aba7f0281d806c5f9907
Arshia Kalkhorani <arshiabdol@yahoo.com>
2021-11-04 16:08:19 -0700

    updated redme file

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ const AppContainer = createAppContainer(AppNavigator);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#c62368',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,1 @@
-React native app
+React native app designed as a companion to my projects website
