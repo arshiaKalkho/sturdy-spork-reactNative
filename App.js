0c647d9e6adabee36509a02eeadb12a9a08ea975
Arshia Kalkhorani <arshiabdol@yahoo.com>
2021-11-04 15:23:02 -0700

    added new random image and background colour

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React  from 'react';
+import { StyleSheet, Text, TouchableOpacity, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
+
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      
+      <Text numberOfLines={4} >Open up App.js to start working on your app!!! Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ipsum officiis iure, aperiam vel quibusdam, rem error pariatur labore ea non harum optio eaque! Dolorem aut sed repellendus in, repellat laborum quod architecto, vitae minima harum earum placeat, quaerat accusantium et sequi magni molestias nisi reprehenderit cum! Ipsa accusantium quo iusto sed distinctio quia aspernatur et voluptatibus libero atque placeat adipisci laudantium error sapiente optio amet  </Text>
+      <TouchableOpacity>
+      <Image
+      source={{
+        width:480,
+        height:270,
+        uri: "https://picsum.photos/480/270"}}/>
+
+      </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#001220',
     alignItems: 'center',
     justifyContent: 'center',
   },
